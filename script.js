464cd418656901b39febda8890248a60bdd0f17a
Jarret Jheng Ch'ng <jarretchng02@gmail.com>
2021-05-15 14:05:40 +1000

    Added dynamic banners

---
+++ script.js
@@ -0,0 +1,6 @@
+﻿const NUMBER_OF_IMAGES = 19; // magic number much
+document.getElementsByClassName("heading")[0].style.backgroundImage = "url('./img/img" + Math.round(Math.random()*NUMBER_OF_IMAGES) + ".jpg')" 
+document.getElementsByClassName("heading")[0].style.backgroundPositionY = Math.ceil(Math.random()*100) + "%";
+// background-size: 100%;
+// background-position-y: 100%;
+  

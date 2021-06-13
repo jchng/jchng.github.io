32c314a206dd8e17be0d04e3aa42927a484d51f5
Jarret Jheng Ch'ng <jarretchng02@gmail.com>
2021-06-13 13:34:35 +1000

    Remove random leg pic

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 ﻿const NUMBER_OF_IMAGES = 19; // magic number much
-document.getElementsByClassName("heading")[0].style.backgroundImage = "url('./img/img" + Math.round(Math.random()*NUMBER_OF_IMAGES) + ".jpg')" 
+const LEG_INDEX = 14;
+
+var randomPicIdx = Math.round(Math.random()*NUMBER_OF_IMAGES);
+// I want to skip that image with my legs in it, makes the website yucky!
+randomPicIdx = (randomPicIdx === LEG_INDEX ? LEG_INDEX - 1 : randomPicIdx)
+
+document.getElementsByClassName("heading")[0].style.backgroundImage = "url('./img/img" + randomPicIdx  + ".jpg')" 
 document.getElementsByClassName("heading")[0].style.backgroundPositionY = Math.ceil(Math.random()*100) + "%";
 // background-size: 100%;
 // background-position-y: 100%;

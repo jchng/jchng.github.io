c55f153aa1912d7925a021299a7d43c2fddf6d4d
Jarret Chng <jarret.chng@wsp.com>
2021-09-04 17:06:31 +1000

    Add images

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-﻿const NUMBER_OF_IMAGES = 19; // A wonderful magic number!
-const LEG_INDEX = 14;
+﻿const NUMBER_OF_IMAGES = 26; // A wonderful magic number!
+const LEG_INDEX = 23;
 var randomPictureIndex = Math.round(Math.random()*NUMBER_OF_IMAGES);
 // I want to skip that image with my legs in it, makes the banner yucky!
 randomPictureIndex = (randomPictureIndex === LEG_INDEX ? LEG_INDEX - 1 : randomPictureIndex);

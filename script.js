c03b6a7f16525c52eced6fc099fb7831d062119c
Jarret Chng <jarret.chng@wsp.com>
2021-09-04 16:58:23 +1000

    Cleanup `script.js`

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-﻿const NUMBER_OF_IMAGES = 19; // magic number much
+﻿const NUMBER_OF_IMAGES = 19; // A wonderful magic number!
 const LEG_INDEX = 14;
-
-var randomPicIdx = Math.round(Math.random()*NUMBER_OF_IMAGES);
-// I want to skip that image with my legs in it, makes the website yucky!
-randomPicIdx = (randomPicIdx === LEG_INDEX ? LEG_INDEX - 1 : randomPicIdx)
-
-document.getElementsByClassName("heading")[0].style.backgroundImage = "url('./img/img" + randomPicIdx  + ".jpg')" 
+var randomPictureIndex = Math.round(Math.random()*NUMBER_OF_IMAGES);
+// I want to skip that image with my legs in it, makes the banner yucky!
+randomPictureIndex = (randomPictureIndex === LEG_INDEX ? LEG_INDEX - 1 : randomPictureIndex);
+document.getElementsByClassName("heading")[0].style.backgroundImage = "url('./img/img" + randomPictureIndex  + ".jpg')" 
 document.getElementsByClassName("heading")[0].style.backgroundPositionY = Math.ceil(Math.random()*100) + "%";
-// background-size: 100%;
-// background-position-y: 100%;
-  

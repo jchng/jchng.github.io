4d0fca623c61d252f1d2ee2808cd00a0c98f9127
Jarret Chng <jarret.chng@wsp.com>
2023-01-04 10:31:53 +1100

    Minor updates to what I do

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const LEG_INDEX = 23;
 var randomPictureIndex = Math.round(Math.random()*NUMBER_OF_IMAGES);
 // I want to skip that image with my legs in it, makes the banner yucky!
 randomPictureIndex = (randomPictureIndex === LEG_INDEX ? LEG_INDEX - 1 : randomPictureIndex);
-document.getElementsByClassName("heading")[0].style.backgroundImage = "url('./img/img" + randomPictureIndex  + ".jpg')" 
+document.getElementsByClassName("heading")[0].style.backgroundImage = "url('./img/img" + randomPictureIndex  + ".jpg')";
 document.getElementsByClassName("heading")[0].style.backgroundPositionY = Math.ceil(Math.random()*100) + "%";

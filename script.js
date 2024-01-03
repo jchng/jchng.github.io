43101a74b3857390ef08c9ff51451c4b1e11c0b0
Jarret Chng <jarret.chng@wsp.com>
2024-01-03 08:48:46 +0800

    Migrate to webp

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-﻿import { BASE_R2_BUCKET_URL, R2_IMAGES_PATH_SUFFIX_KEY } from './constants.js';
+﻿import { BASE_R2_BUCKET_URL, R2_WEBP_IMAGES_PATH_SUFFIX_KEY } from './constants.js';
 import { imgCountFromBucket, zeroPad } from './utils.js';
 
 // I want to skip that image with my legs in it, makes the banner yucky!
@@ -8,6 +8,6 @@ const LEG_INDEX = 23;
 var randomPictureIndex = Math.ceil(Math.random() * NUMBER_OF_IMAGES);
 randomPictureIndex = (randomPictureIndex === LEG_INDEX ? LEG_INDEX - 1 : randomPictureIndex);
 
-const bannerImgUrl = BASE_R2_BUCKET_URL + R2_IMAGES_PATH_SUFFIX_KEY + zeroPad(randomPictureIndex, 3) + ".jpg"
+const bannerImgUrl = BASE_R2_BUCKET_URL + R2_WEBP_IMAGES_PATH_SUFFIX_KEY + zeroPad(randomPictureIndex, 3) + ".webp"
 document.getElementsByClassName("heading")[0].style.backgroundImage = "url('" + bannerImgUrl + "')";
 document.getElementsByClassName("heading")[0].style.backgroundPositionY = Math.ceil(Math.random()*100) + "%";

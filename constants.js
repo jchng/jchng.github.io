394e6934fe0ea3fd91c1fffec1c0832dbcce7bfc
Jarret Chng <jarret.chng@wsp.com>
2024-12-24 08:14:02 +1100

    Move to jchng.com

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-export const BASE_R2_BUCKET_URL = "https://r2.jarretch.ng/";
+export const BASE_R2_BUCKET_URL = "https://r2.jchng.com/";
 export const R2_IMAGES_PATH_SUFFIX_KEY = "imgs/";
 export const R2_WEBP_IMAGES_PATH_SUFFIX_KEY = R2_IMAGES_PATH_SUFFIX_KEY + "webps/";
 export const R2_THUMBNAIL_PATH_SUFFIX_KEY = R2_IMAGES_PATH_SUFFIX_KEY + "thumbs/";
